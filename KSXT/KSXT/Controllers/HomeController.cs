e3bae57a3c6634bf240ef15bc776f61fcd648e75
Paige <2624400269@qq.com>
2021-10-25 11:52:02 +0800

    考试系统 创建1

---
+++ KSXT/KSXT/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KSXT.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 登录界面（Login）
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+
+
+        {
+            return View();
+        }
+    }
+}
---
+++ KSXT/KSXT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KSXT.MvcApplication" Language="C#" %>
